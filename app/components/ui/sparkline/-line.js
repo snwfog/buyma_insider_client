9c24b9d9e091cf1fd43efe4d586152d49d7727e6
Charles Yang <donchoa@gmail.com>
2016-11-12 20:12:19 -0500

    Add proper graph for item price

--- app/components/ui/sparkline/-line.js
+++ app/components/ui/sparkline/-line.js
@@ -3,7 +3,16 @@ import UiSparklineComponent from '../-sparkline';
 
 export default UiSparklineComponent.extend({
   options: {
-    type:   'line',
-    height: 40,
+    type:               'line',
+    height:             40,
+    lineColor:          '#00d1b2',
+    lineWidth:          2,
+    fillColor:          null,
+    spotColor:          '#ff3860',
+    minSpotColor:       '#ff3860',
+    maxSpotColor:       '#ff3860',
+    highlightSpotColor: '#ff3860',
+    highlightLineColor: '#ff3860',
+    chartRangeMin:      0,
   },
 });
