f023c21a6cbbef4527375d2e3e9afc6aa0d4ed8b
Charles Yang <donchoa@gmail.com>
2016-11-17 23:12:23 -0500

    Start paging

--- app/routes/merchant/index.js
+++ app/routes/merchant/index.js
@@ -1,17 +1,31 @@
 import Ember from 'ember';
 
-const { hash } = Ember.RSVP;
+const { merge, getWithDefault } = Ember;
+const { hash }                  = Ember.RSVP;
 
 export default Ember.Route.extend({
-  model() {
+  model(params) {
     var { metadatum } = this.modelFor('merchant');
     return hash({
-      articles: this.store.query('article', { merchant: metadatum.id }),
+      articles: this.store.query('article',
+        merge({ merchant: metadatum.id }, params)),
     });
   },
 
   setupController(controller, models) {
     this._super(...arguments);
     controller.setProperties(models);
+
+    controller.reopen({
+      queryParams: ['page', 'count'],
+      page:        getWithDefault(controller, 'page', 1),
+    });
+  },
+
+  resetController(controller, isExiting)  {
+    this._super(...arguments);
+    if (isExiting) {
+      controller.set('page', 1);
+    }
   }
 });
