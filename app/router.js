ac0c48d5d11503263ea55439460853d7d5c6b191
Charles Yang <charles.y.yang@oracle.com>
2017-05-05 11:44:27 -0400

    cleanup routes

--- app/router.js
+++ app/router.js
@@ -13,22 +13,24 @@ Router.map(function () {
   this.route('login');
   this.route('logout');
   this.route('signup');
-  this.route('search');
   this.route('estimator');
 
-  this.route('dashboard');
-  this.route('dashboard_archive', { path: '/archive' }, function() {
-    this.route('year',    { path: '/year/:year_id'   }, function() {
-      this.route('month', { path: '/month/:month_id' }, function() {
-        this.route('day', { path: '/day/:day_id'     });
+  this.route('index', { path: '/' }, function() {
+    this.route('dashboard');
+    this.route('dashboard_archive', { path: '/archive' }, function() {
+      this.route('year',    { path: '/year/:year_id'   }, function() {
+        this.route('month', { path: '/month/:month_id' }, function() {
+          this.route('day', { path: '/day/:day_id'     });
+        });
       });
     });
   });
 
   this.route('status');
   this.route('articles', { path: '/articles/:article_id' }, function() {
-//     this.route('index');
+    //     this.route('index');
     this.route('sold', { path: '/sold/:user_article_sold_id' });
+    this.route('_search');
   });
 
   this.route('merchant', { path: '/:merchant_id' }, function () {
