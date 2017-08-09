b4d6aab610985b4c95128ba11e61f51bb2b5987b
Charles Yang <donchoa@gmail.com>
2017-08-09 18:52:45 -0400

    fix link icon alignment

---
+++ app/components/ui/-side-menu-link-to.js
@@ -0,0 +1,6 @@
+import SideMenuLinkToComponent from 'ember-side-menu/components/side-menu-link-to';
+
+export default SideMenuLinkToComponent.extend({
+  classNames:  'align-fix',
+  activeClass: 'is-active'
+});
